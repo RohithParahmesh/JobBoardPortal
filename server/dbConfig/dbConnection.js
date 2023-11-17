556e84a1a8399baa448e16af34fd2ecba22e13b4
Rohith parahmesh <rohithparahmesh@gmail.com>
2023-11-17 13:22:44 +0530

    Add job card issue code

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbConnection = async()=>{
     try{
-        const dbConnection = await mongoose.connect("")
+        const dbConnection = await mongoose.connect("mongodb+srv://rohithp:rohithp@cluster0.jtbonvk.mongodb.net/?retryWrites=true&w=majority")
         console.log("Database connected")
     }
 
